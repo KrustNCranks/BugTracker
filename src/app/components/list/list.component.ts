def261960599f7bebf58b826401ccbb37b4766e1
Chathu Abeywickrama <chathu.git93@gmail.com>
2018-09-24 10:32:48 +0530

    frontend successfully retrieving data from the back end

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IssueService } from "../../issue.service";
 
 @Component({
   selector: 'app-list',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  /**
+   * Adding an Instance of the IssueService
+   * @param issueService
+   */
+  constructor(private issueService: IssueService) { }
 
   ngOnInit() {
+    this.issueService.getIssues().subscribe((issues)=>{
+      console.log(issues);
+    })
   }
 
 }
